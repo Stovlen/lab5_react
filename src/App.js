0ed4104b90e2aca7d6ece461d617ca59d4e82551
Olexandr Stovban <stovbanolexandr@gmail.com>
2024-06-05 10:25:32 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Menu from "./components/menu/Menu";
+import ShopCard from "./components/shop-card/ShopCard";
 
 function App() {
   return (
@@ -7,6 +8,19 @@ function App() {
       <header className="app-header">
         <Menu />
       </header>
+
+      <main>
+        <section className="container">
+          <div className="row">
+            <div className="col-12 col-lg-3">
+              <ShopCard
+                card={{ id: 1, title: "title 1", description: "description 1" }}
+                otherval={"other"}
+              />
+            </div>
+          </div>
+        </section>
+      </main>
     </section>
   );
 }
